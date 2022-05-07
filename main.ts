19e5931af07da110871be19f6ea4dbb157b1c3e3
ILOVEZCATS <104972960+ILOVEZCATS@users.noreply.github.com>
2022-05-07 10:53:16 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,58 @@
+let Roll = 0
+input.onGesture(Gesture.Shake, function () {
+    Roll = randint(1, 6)
+    if (Roll == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (Roll == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    if (Roll == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    if (Roll == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (Roll == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (Roll == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
